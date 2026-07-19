fe869cf4a69d260560b03fabc61892d79c7e4f1a
zaqueourico2020-ship-it <zaqueourico2020@gmail.com>
2026-07-19 20:55:26 -0300

    Add Categories and FeaturedProducts components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
 import HeroBanner from "../components/HeroBanner";
+import Categories from "../components/Categories";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 export default function Home() {
   return (
@@ -8,12 +10,14 @@ export default function Home() {
       style={{
         maxWidth: "1200px",
         margin: "0 auto",
-        padding: "16px",
+        padding: "16px"
       }}
     >
       <Header />
       <SearchBar />
       <HeroBanner />
+      <Categories />
+      <FeaturedProducts />
     </main>
   );
 }
