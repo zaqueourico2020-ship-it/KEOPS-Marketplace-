e52a7c5ac9dbfa154f6609ddb12c66193470a34b
zaqueourico2020-ship-it <zaqueourico2020@gmail.com>
2026-07-19 21:46:06 -0300

    Integrate Login component into Home page
    Added Login component to the main page layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,20 @@ import SearchBar from "../components/SearchBar";
 import HeroBanner from "../components/HeroBanner";
 import Categories from "../components/Categories";
 import FeaturedProducts from "../components/FeaturedProducts";
+import Login from "../components/Login";
 
 export default function Home() {
   return (
-    <main
-      style={{
-        maxWidth: "1200px",
-        margin: "0 auto",
-        padding: "16px"
-      }}
-    >
+    <main>
       <Header />
       <SearchBar />
       <HeroBanner />
       <Categories />
       <FeaturedProducts />
+
+      <div style={{ marginTop: "40px" }}>
+        <Login />
+      </div>
     </main>
   );
 }
