69c6e614be875566116ad4caf4a171648be5c47a
zaqueourico2020-ship-it <zaqueourico2020@gmail.com>
2026-07-19 22:19:55 -0300

    Create SearchPage component for product search

---
+++ src/app/search/page.tsx
@@ -0,0 +1,8 @@
+export default function SearchPage() {
+  return (
+    <main style={{ padding: "30px" }}>
+      <h1>🔍 Buscar</h1>
+      <p>Pesquise produtos, lojas e categorias.</p>
+    </main>
+  );
+}
